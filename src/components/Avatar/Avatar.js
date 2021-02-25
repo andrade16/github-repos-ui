a33eb61886d978146269193a3d926c37b11c0790
William Andrade <andrade.william61@gmail.com>
2021-02-25 14:30:40 -0800

    ternary operator for Avatar

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -2,19 +2,16 @@ import React from "react";
 import "./Avatar.scss";
 
 const Avatar = ({ src, width, height, icon }) => {
-  let inner = null;
-  if (src) {
-    inner = (
-      <img
-        src={src}
-        alt="avatar-image"
-        style={{ width: width, height: height }}
-        className="avatar-image"
-      />
-    );
-  } else {
-    inner = <div className="avatar-image">{icon}</div>;
-  }
+  let inner = src ? (
+    <img
+      src={src}
+      alt="avatar-image"
+      style={{ width: width, height: height }}
+      className="avatar-image"
+    />
+  ) : (
+    <div className="avatar-image">{icon}</div>
+  );
 
   return <div className="avatar">{inner}</div>;
 };
